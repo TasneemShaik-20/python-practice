b06f1c7b83d1d100cc075442b40df13dc6b9afb6
Tasneem Shaik <shaiktasneem918@gmail.com>
2026-02-06 19:50:52 +0530

    init commit

---
+++ chapter3prac.py
@@ -0,0 +1,34 @@
+# a=input("type the name")
+
+# # print(a,"good morning!")
+# date=input("date")
+
+# print("Dear ",a,"/n You are slected !\n ," ,date)
+
+name="harry"
+print(name[0])
+# name[0]="t"
+print(name)
+
+string="hello wold"
+print(string[0:7:2])
+print(string[-1])
+
+# name="harry"
+# name[0]="p"
+# print(name)
+
+list=["helo",123,67.87,"mango"]
+list[2]=67
+print(list)
+li=[1,2,3,4]
+li.append(5)
+print(li)
+tup=("tina","priya","rashi")
+# tup(0)="anjali"
+print(type(tup))
+tup=(1,)
+print(type(tup))
+print(sum(li))
+li2=[1,2,3,1,2,1,1]
+print(li2.count(1))
---
+++ problem1.py
@@ -0,0 +1,22 @@
+# name=input("enter your name")
+# print(name)
+# 
+
+# x=input("type somethn ")
+# y=float(input())
+# z=int(input())
+
+# print(type(x))
+# print(type(y))
+# print(type(z))
+
+# 
+
+# p=int(input())
+# q=int(input())
+# print("the sum of given two numbers is :",p+q)
+
+
+num=int(input("given number "))
+sqrt=num*num
+print("the square of the given number is ",sqrt)
