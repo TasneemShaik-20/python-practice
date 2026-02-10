31710846917f5708322fd5966b57525645f0cd1c
Tasneem Shaik <shaiktasneem918@gmail.com>
2026-02-10 19:04:08 +0530

    loops in Python

---
+++ loops.py
@@ -0,0 +1,14 @@
+# i=0
+# for i in range(0,5):
+#     print(i)
+
+# for i in range(5):
+#     print (i)   
+l=[1,"harry",56,"hello",7689,"abc"]
+for i in l:
+    print(i)
+
+t=("nimisha","atul","rohan","priya")
+
+for name in t:
+    print(name)
