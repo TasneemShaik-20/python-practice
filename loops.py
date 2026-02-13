f7ed07557ccc6484c4b601e677d44dac1a20e88e
Tasneem Shaik <shaiktasneem918@gmail.com>
2026-02-13 16:29:27 +0530

    Python Project(Snake,Water,Gun)

--- loops.py
+++ loops.py
@@ -4,11 +4,27 @@
 
 # for i in range(5):
 #     print (i)   
-l=[1,"harry",56,"hello",7689,"abc"]
-for i in l:
-    print(i)
+# l=[1,"harry",56,"hello",7689,"abc"]
+# for i in l:
+#     print(i)
 
-t=("nimisha","atul","rohan","priya")
+# t=("nimisha","atul","rohan","priya")
 
-for name in t:
-    print(name)
+# for name in t:
+#     print(name)
+
+# for i in range(4):
+#     print("hello")
+#     if(i==3):
+#         break
+
+# for i in range(4):
+#     if(i==2):
+#         continue
+#     print("hello")
+#     print(i)
+
+n=int(input("enter the value"))
+
+for i in range(1,11):
+    print(f"{n}X{i}={n*i}")
